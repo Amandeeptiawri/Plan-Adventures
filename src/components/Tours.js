2ffa3382e6385f02a1358cfbb6da43c229c1e4b5
Amandeeptiwri <amandeeptiawri@gmail.com>
2024-05-23 04:49:29 +0530

    first commit

---
+++ src/components/Tours.js
@@ -0,0 +1,29 @@
+import Card from "./Card"
+
+function Tours({tour,removeTour}){
+return(
+    <div>
+    <div>
+    <h2> Plan With Love</h2>
+    </div>
+
+    <div>
+
+    {
+            tour.map((tour)=>{
+                return(<Card key={tour.id} {...tour} removeTour={removeTour}></Card>)
+
+            })
+    }
+    
+
+    </div>
+
+
+    </div>
+
+);
+
+}
+
+export default Tours;
